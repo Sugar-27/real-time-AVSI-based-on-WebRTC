e7227ccd7a233d56844d4c0fc4b25f654ee6af0f
Sugar <45682h@gmail.com>
2022-11-13 21:17:54 +0800

    增加push的html模版

--- src/action/xrtcclient_push.go
+++ src/action/xrtcclient_push.go
@@ -3,6 +3,7 @@ package action
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 type xrtcClientPushAction struct{}
@@ -11,6 +12,31 @@ func NewXrtcClientPushAction() *xrtcClientPushAction {
 	return &xrtcClientPushAction{}
 }
 
-func (*xrtcClientPushAction) Execute(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("hello xrtcclient push action")
+func writeHtmlErrorResponse(w http.ResponseWriter, status int, err string) {
+	w.WriteHeader(status)
+	w.Write([]byte(err))
 }
+
+
+func (*xrtcClientPushAction) Execute(w http.ResponseWriter, r *http.Request) {
+	// fmt.Println("hello xrtcclient push action")
+	t, err := template.ParseFiles("./static/template/push.tpl")
+	if err != nil {
+		fmt.Println(err)
+		writeHtmlErrorResponse(w, http.StatusNotFound, "404 - Not found")
+		return
+	}
+
+	request := make(map[string]string)
+
+	for k, v := range r.Form {
+		request[k] = v[0]
+	}
+
+	t.Execute(w, request)
+	if err != nil {
+		writeHtmlErrorResponse(w, http.StatusNotFound, "404 - Not found")
+		return
+	}
+}
+
